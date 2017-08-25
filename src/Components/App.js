87f790a444074c24c69be7a33039b9a6edde129c
Shinye Song <novembertwentysix@gmail.com>
2017-08-26 00:24:08 +0900

    boilerplate setting

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import OrigSection from './OrigSection';
+import logo from '../logo.svg';
+import './css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <OrigSection />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/OrigSection.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './css/OrigSection.css';
+
+class OrigSection extends Component {
+  render() {
+    return (
+      <div className="File-add-icon">
+        <h2>origsection test....</h2>
+      </div>
+    );
+  }
+}
+
+export default OrigSection;
